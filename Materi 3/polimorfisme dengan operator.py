168a0c4e207d8b056c3c0a6479f97c344ad76f7b
delladeldel <2215061116@students.ac.id>
2024-05-09 20:48:01 +0700

    menambahkan file

---
+++ Materi 3/polimorfisme dengan operator.py
@@ -0,0 +1,11 @@
+# operator + pada tipe data int
+x = 5
+y = 10
+z = x + y
+print(z)  # output: 15
+
+# operator + pada tipe data string
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)  # output: Hello World
