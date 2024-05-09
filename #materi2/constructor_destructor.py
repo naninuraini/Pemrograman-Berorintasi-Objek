063cfaa22c8b86260312a17c411f226be2ad574a
naninuraini <naninuraini1023@gmail.com>
2024-05-09 20:19:19 +0700

    mengapus file

--- #materi2/constructor_destructor.py
+++ #materi2/constructor_destructor.py
@@ -12,17 +12,17 @@ class Manusia:
 p = Manusia("John Doe", 25)
 p.info()
 
-#Penggunaan dectructor
-class Mobil:
-    def __init__(self, merek, warna):
-        self.merek = merek
-        self.warna = warna
+# #Penggunaan dectructor
+# class Mobil:
+#     def __init__(self, merek, warna):
+#         self.merek = merek
+#         self.warna = warna
 
-    def __del__(self):
-        print(f"{self.merek} {self.warna} dihapus")
+#     def __del__(self):
+#         print(f"{self.merek} {self.warna} dihapus")
 
-mobil1 = Mobil("Toyota", "Merah")
-del mobil1
+# mobil1 = Mobil("Toyota", "Merah")
+# del mobil1
 
 
 
